2f9960924bc14eccf3ff3058a71843273d220aa4
Olivier Le Guyader <olivier.leguyader@alcuin.com>
2021-09-22 14:43:13 +0200

    1er TU

---
+++ Bank/Account/Account.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Threading.Tasks;
+
+using Bank.Exceptions;
+
+namespace Bank.Account
+{
+    public class Account : IAccount
+    {
+        public int AccountNumber { get; } = 0;
+		
+		public decimal OverdraftLimit { get; set; }
+		
+        public decimal Balance { get; } = 0;
+		
+        public async Task DepositAsync(decimal amount)
+        {
+			if (amount < 0) throw new UnauthorizedAccountOperationException();
+        }
+
+		public async Task WithdrawAsync(decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
